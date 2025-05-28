304edb34bbcad50bc4fd29f83c34af24ddbd047d
ORIGAMI7023 <142854552+ORIGAMI7023@users.noreply.github.com>
2025-05-28 22:49:55 +0800

    Mobile 开始构建Doctor页面

--- FMS.Mobile.py
+++ FMS.Mobile.py
@@ -20,6 +20,9 @@ target_folders = [
     "Controls",
 ]
 
+all_paths = []  # 记录所有相对路径
+
+
 with open(output_file, "w", encoding="utf-8") as out:
     for dirpath, _, filenames in os.walk(root_dir):
         rel_dir = os.path.relpath(dirpath, root_dir)
@@ -39,8 +42,17 @@ with open(output_file, "w", encoding="utf-8") as out:
 
                 file_path = os.path.join(dirpath, filename)
                 rel_path = os.path.relpath(file_path, root_dir)
+                all_paths.append(rel_path)
                 out.write(f"\n\n# --- {rel_path} ---\n")
                 with open(file_path, "r", encoding="utf-8", errors="ignore") as f:
                     out.write(f.read())
+
+
+    # 写入文件索引列表到文件头
+    out.seek(0, 0)
+    out.write("### 文件索引\n")
+    out.write("\n".join(all_paths))
+    out.write("\n\n### 文件内容\n")
+
 
 print(f"已合并导出到 {output_file}")
--- FMS.Server.py
+++ FMS.Server.py
@@ -8,6 +8,9 @@ target_exts = [".cs", ".json", ".http", ".csproj"]
 # 只保留这些目录下的 .cs 文件
 target_folders = ["Controllers", "Data", "Models", "Migrations","Converter"]
 
+all_paths = []  # 记录所有相对路径
+
+
 with open(output_file, "w", encoding="utf-8") as out:
     for dirpath, _, filenames in os.walk(root_dir):
         # 筛选只遍历目标目录，或者根目录
@@ -21,7 +24,16 @@ with open(output_file, "w", encoding="utf-8") as out:
                 (rel_dir != "." and ext == ".cs")):
                 file_path = os.path.join(dirpath, filename)
                 rel_path = os.path.relpath(file_path, root_dir)
+                all_paths.append(rel_path)
                 out.write(f"\n\n# --- {rel_path} ---\n")
                 with open(file_path, "r", encoding="utf-8", errors="ignore") as f:
                     out.write(f.read())
+                    
+                    
+    # 写入文件索引列表到文件头
+    out.seek(0, 0)
+    out.write("### 文件索引\n")
+    out.write("\n".join(all_paths))
+    out.write("\n\n### 文件内容\n")
+    
 print(f"已合并导出到 {output_file}")
